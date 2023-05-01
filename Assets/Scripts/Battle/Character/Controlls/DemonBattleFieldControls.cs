732ecfc6641d88b7280f2299e6866cd9fb7ee911
Nimiod <Nimiod02@gmail.com>
2023-05-01 14:39:12 +0700

    Feat(Demon) - полное управление демоном

--- Assets/Scripts/Battle/Character/Controlls/DemonBattleFieldControls.cs
+++ Assets/Scripts/Battle/Character/Controlls/DemonBattleFieldControls.cs
@@ -3,43 +3,65 @@ using UnityEngine.EventSystems;
 
 public class DemonBattleFieldControls : AbstractBattleFieldControls
 {
-    private Vector2Int? _lastClickedIndex;
-    
+    #region CellControls
+
     protected override void OnCellClicked(PointerEventData data, Vector2Int index)
     {
-        if (_lastClickedIndex == null)
+        if(IsNotInteractable(index)) return;
+
+        if (SelectedIndex == null)
         {
-            _lastClickedIndex = index;
+            SelectedIndex = index;
             return;
         }
 
-        if (_lastClickedIndex == index)
+        if (SelectedIndex == index)
         {
-            _lastClickedIndex = null;
+            SelectedIndex = null;
             return;
         }
 
-        CrystalField.SwitchCrystals(_lastClickedIndex.Value, index);
-        _lastClickedIndex = null;
+        CrystalField.SwitchCrystals(SelectedIndex.Value, index);
+        CrystalField.GetCrystal(SelectedIndex.Value).ChangeState(Crystal.ConditionState.Cursed);
+        CrystalField.GetCrystal(index).ChangeState(Crystal.ConditionState.Cursed);
+        SelectedIndex = null;
     }
 
 
     protected override void OnCellDragStarted(PointerEventData data, Vector2Int index)
     {
+        if(IsNotInteractable(index)) return;
+        SelectedIndex = index;
     }
 
     protected override void OnCellDragged(PointerEventData data, Vector2Int index)
     {
+        if(IsNotInteractable(index)) return;
         CrystalField.MakeCrystalFollowMouse(index);
     }
 
     protected override void OnCellDragEnded(PointerEventData data, Vector2Int index)
     {
+        SelectedIndex = null;
         CrystalField.MoveBack(index);
     }
 
     protected override void OnCellDroppedOn(PointerEventData data, Vector2Int targetIndex, Vector2Int droppedIndex)
     {
+        if(IsNotInteractable(droppedIndex) || !IsSettable(targetIndex)) return;
+        SelectedIndex = null;
         CrystalField.SwitchCrystals(targetIndex, droppedIndex);
+        CrystalField.GetCrystal(targetIndex).ChangeState(Crystal.ConditionState.Cursed);
+        CrystalField.GetCrystal(droppedIndex).ChangeState(Crystal.ConditionState.Cursed);
     }
+
+    #endregion
+
+    #region Predicates
+
+    protected override bool IsSettable(Vector2Int index) => SelectedIndex != null &&
+                                                            CrystalField.GetCrystal(index)
+                                                                .CurrentConditionState != Crystal.ConditionState.Cursed;
+
+    #endregion
 }
